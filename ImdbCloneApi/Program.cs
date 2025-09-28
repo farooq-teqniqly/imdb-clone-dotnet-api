c4889885d690983fcedef92097e1f122b76bbf7f
farooq-teqniqly <farooq@teqniqly.com>
2025-09-27 19:03:48 -0700

    feat: Add health checks to the application and update HTTP host address

--- ImdbCloneApi/Program.cs
+++ ImdbCloneApi/Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
@@ -44,6 +45,8 @@ app.MapGet(
     }
   )
   .WithName("GetWeatherForecast");
+
+app.MapHealthChecks("/health");
 
 app.Run();
 
